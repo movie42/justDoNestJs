25813b2df88c9dea428c5801b4bf6a9146d18b7d
HYUNSU <movie3342@gmail.com>
2023-10-25 22:24:24 +0900

    feat: config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,21 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 
-import { AuthService } from "./auth.service";
-import { AuthController } from "./auth.controller";
+import * as config from "config";
 import { TypeOrmExModule } from "src/db/typeorm-ex.module";
-import { UserRepository } from "./user.repository";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 import { JwtStrategy } from "./jwt.strategy";
-
+import { UserRepository } from "./user.repository";
+const jwtConfig = config.get("jwt");
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: "jwt" }),
     JwtModule.register({
-      secret: "SEcReT14!4234",
-      signOptions: { expiresIn: 3600 * 24 }
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN || jwtConfig.expiresIn
+      }
     }),
     TypeOrmExModule.forCustomRepository([UserRepository])
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as config from "config";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 4000;
+  const serverConfig = config.get("server");
+  const port = serverConfig.port;
   await app.listen(port);
   Logger.log(`Application running on port ${port}`);
 }
